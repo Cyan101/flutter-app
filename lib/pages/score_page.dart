ebf16629e0de87835ca45ec414927cd55b7ada27
Jos Spencer <crazyandroid101@gmail.com>
2018-03-10 16:06:20 +0800

    Score page created

---
+++ lib/pages/score_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ScorePage extends StatelessWidget {
+
+  final int score;
+  final int totalQuestions;
+
+  ScorePage(this.score, this.totalQuestions);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      color: Colors.cyanAccent,
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Text("Score: ", style: new TextStyle(color: Colors.white, fontSize: 50.0, fontWeight: FontWeight.bold)),
+          new Text(score.toString() + "/" + totalQuestions.toString(), style: new TextStyle(color: Colors.white, fontSize: 50.0, fontWeight: FontWeight.bold)),
+          new IconButton(
+              icon: new Icon(Icons.redo),
+              color: Colors.white,
+              iconSize: 50.0,
+              onPressed: () => "Resatart"),
+        ],
+      )
+    );
+  }
+}
