e702bcccfee31e34c5fb1bc2e133fc2d25d5d5d1
Jos Spencer <crazyandroid101@gmail.com>
2018-03-21 12:36:03 +0800

    Added comments to some of the code

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -6,13 +6,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
+      title: 'Welcome to Flutter!', // Multitasking title
+      home: new Scaffold( // The Scaffold widget, from the Material library, provides a default app bar, title, and a body property that holds the widget tree for the home screen.
         appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
+          title: new Text('Welcome to Flutter'), // Text at the top title bar of the app
         ),
         body: new Center(
-          child: new Text('Hello World'),
+          child: new Text('Hello World'), // Text in the center of the app
         ),
       ),
     );
