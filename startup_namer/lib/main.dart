3b25248f65dd673db36333698f3ab80c173630f7
Jos Spencer <crazyandroid101@gmail.com>
2018-03-21 13:37:29 +0800

    Randomly generated Wordpair

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter!', // Multitasking title
       home: new Scaffold( // The Scaffold widget, from the Material library, provides a default app bar, title, and a body property that holds the widget tree for the home screen.
         appBar: new AppBar(
+          leading: new IconButton(icon: new Icon(Icons.add_a_photo), onPressed: null, tooltip: '*Snap!*'),
           title: new Text('Welcome to Flutter'), // Text at the top title bar of the app
         ),
         body: new Center(
-          child: new Text('Hello World'), // Text in the center of the app
+          child: new Text(wordPair.asPascalCase), // Text in the center of the app
         ),
       ),
     );
