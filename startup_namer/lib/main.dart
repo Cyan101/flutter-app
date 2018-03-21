b8f759d646ea89648e6351deeabc38deb67f6e6b
Jos Spencer <crazyandroid101@gmail.com>
2018-03-21 13:42:38 +0800

    Use a class to generate a random word pair

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -7,7 +7,6 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter!', // Multitasking title
       home: new Scaffold( // The Scaffold widget, from the Material library, provides a default app bar, title, and a body property that holds the widget tree for the home screen.
@@ -16,9 +15,22 @@ class MyApp extends StatelessWidget {
           title: new Text('Welcome to Flutter'), // Text at the top title bar of the app
         ),
         body: new Center(
-          child: new Text(wordPair.asPascalCase), // Text in the center of the app
+          child: new RandomWords(), // Text in the center of the app
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
